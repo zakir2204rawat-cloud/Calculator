73efb4e296133da975f8b1be475f1685ceb9377e
zakir2204rawat-cloud <zakir2204rawat@gmail.com>
2026-08-08 22:44:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,79 @@
 const display = document.getElementById("display");
 
+
+// Add numbers and operators to display
 function appendValue(value) {
     display.value += value;
 }
 
+
+// Clear everything
 function clearDisplay() {
     display.value = "";
 }
 
+
+// Calculate answer
 function calculate() {
+
     try {
+
+        if (display.value === "") {
+            display.value = "";
+            return;
+        }
+
         display.value = eval(display.value);
-    } catch (error) {
+
+    } 
+    catch (error) {
+
         display.value = "Error";
+
     }
+
 }
+
+
+// Keyboard support
+document.addEventListener("keydown", function(event){
+
+    const key = event.key;
+
+
+    if(
+        key >= "0" && 
+        key <= "9" || 
+        key === "." ||
+        key === "+" ||
+        key === "-" ||
+        key === "*" ||
+        key === "/"
+    ){
+
+        appendValue(key);
+
+    }
+
+
+    else if(key === "Enter"){
+
+        calculate();
+
+    }
+
+
+    else if(key === "Escape"){
+
+        clearDisplay();
+
+    }
+
+
+    else if(key === "Backspace"){
+
+        display.value = display.value.slice(0,-1);
+
+    }
+
+});
